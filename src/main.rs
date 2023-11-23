6de8fa07d67051b0ac1984dd66ffbfe9f2a7a10f
Javier Cardona <javier@cardonabits.com>
2023-11-23 18:31:57 +0000

    Attached SCSI disk successfully
    Fails to mount due to unable to read superblock

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,45 @@ mod voltage;
 
 use stm32l0xx_hal as hal;
 
+#[derive(Default)]
+struct State {
+    storage_offset: usize,
+    sense_key: Option<u8>,
+    sense_key_code: Option<u8>,
+    sense_qualifier: Option<u8>,
+}
+
+impl State {
+    fn reset(&mut self) {
+        self.storage_offset = 0;
+        self.sense_key = None;
+        self.sense_key_code = None;
+        self.sense_qualifier = None;
+    }
+}
+
 #[rtic::app(device = stm32l0xx_hal::pac, dispatchers = [RTC])]
 mod app {
 
-    use  core::mem::MaybeUninit;
+    use core::mem::MaybeUninit;
 
     const USB_PACKET_SIZE: u16 = 64; // 8,16,32,64
     static mut USB_TRANSPORT_BUF: MaybeUninit<[u8; 512]> = MaybeUninit::uninit();
     const BLOCK_SIZE: u32 = 512;
+    const BLOCKS: u32 = 1;
     const MAX_LUN: u8 = 0; // max 0x0F
+                           // XXX: This is the actual file content.  It needs to be replaced by flash
+    static mut STORAGE: [u8; (BLOCKS * BLOCK_SIZE) as usize] =
+        [0u8; (BLOCK_SIZE * BLOCKS) as usize];
+
+    static mut STATE: State = State {
+        storage_offset: 0,
+        sense_key: None,
+        sense_key_code: None,
+        sense_qualifier: None,
+    };
+
+    use super::*;
     use crate::{
         config::{self, FlashConfig, QAType},
         display::show_q_or_a,
@@ -40,9 +70,12 @@ mod app {
             syscfg::SYSCFG,
             usb::{UsbBus, USB},
         },
-        voltage::VoltageLevels::High
+        voltage::VoltageLevels::High,
     };
-    use epd_waveshare::{epd1in54_v2::{*, Display1in54}, prelude::*};
+    use epd_waveshare::{
+        epd1in54_v2::{Display1in54, *},
+        prelude::*,
+    };
     use hex_display::HexDisplayExt;
     use rtic_sync::channel::Receiver;
     use rtic_sync::{channel::*, make_channel};
@@ -50,9 +83,9 @@ mod app {
     use shared_bus::{BusManager, NullMutex, SpiProxy};
     use usb_device::{
         bus::UsbBusAllocator,
-        prelude::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
+        prelude::{UsbDevice, UsbDeviceBuilder, UsbDeviceState, UsbVidPid},
     };
-    use usbd_storage::{subclass::scsi::{Scsi, ScsiCommand}};
+    use usbd_storage::subclass::scsi::{Scsi, ScsiCommand};
     use usbd_storage::subclass::Command;
     use usbd_storage::transport::bbb::{BulkOnly, BulkOnlyError};
     use usbd_storage::transport::TransportError;
@@ -71,8 +104,7 @@ mod app {
     type BusMgr = BusManager<BusMgrInner>;
 
     #[shared]
-    struct Shared {
-    }
+    struct Shared {}
 
     #[local]
     struct Local {
@@ -150,9 +182,12 @@ mod app {
             "cardonabits.github.io/lightnote-app/",
         );
 
-        let mut scsi = usbd_storage::subclass::scsi::Scsi::new(usb_bus.as_ref().unwrap(), USB_PACKET_SIZE, MAX_LUN, unsafe {
-            USB_TRANSPORT_BUF.assume_init_mut().as_mut_slice()
-        })
+        let mut scsi = usbd_storage::subclass::scsi::Scsi::new(
+            usb_bus.as_ref().unwrap(),
+            USB_PACKET_SIZE,
+            MAX_LUN,
+            unsafe { USB_TRANSPORT_BUF.assume_init_mut().as_mut_slice() },
+        )
         .unwrap();
 
         let usb_dev = UsbDeviceBuilder::new(usb_bus.as_ref().unwrap(), UsbVidPid(0xf055, 0xdf11))
@@ -192,7 +227,12 @@ mod app {
         let flash = cx.local.flash;
 
         // XXX: Until we read it from flash
-        let config = FlashConfig { page_size: 8192, num_pages: 100, q_type: config::QAType::RawImage, a_type: QAType::Text };
+        let config = FlashConfig {
+            page_size: 8192,
+            num_pages: 100,
+            q_type: config::QAType::RawImage,
+            a_type: QAType::Text,
+        };
         loop {
             // show_q_or_a(
             //     epd,
@@ -203,7 +243,7 @@ mod app {
             //     &config,
             //     0x0000_0000,
             //     display,
-            //     false 
+            //     false
             // ).ok();
             rprintln!("epd stuff here...");
             delay.delay_ms(1000u32);
@@ -224,7 +264,24 @@ mod app {
         led.toggle().ok();
 
         let usb_dev = cx.local.usb_dev;
-        usb_dev.poll(&mut [cx.local.webusb, cx.local.scsi]);
+        if !usb_dev.poll(&mut [cx.local.webusb, cx.local.scsi]) {
+            // No USB work is required
+            return;
+        }
+
+        // clear state if just configured or reset
+        if matches!(usb_dev.state(), UsbDeviceState::Default) {
+            unsafe {
+                STATE.reset();
+            };
+        }
+
+        let _ = cx.local.scsi.poll(|command| {
+            led.set_low();
+            if let Err(err) = process_scsi_command(command) {
+                rprintln!("scsi poll error");
+            }
+        });
         rprintln!("USB interrupt done");
     }
 
@@ -237,5 +294,148 @@ mod app {
             return format_no_std::show(&mut SERIAL_NUM, format_args!("{}", THIS_DEVICE_ID.hex()))
                 .unwrap();
         }
+    }
+
+    fn process_scsi_command(
+        mut command: Command<ScsiCommand, Scsi<BulkOnly<UsbBus<USB>, &mut [u8]>>>,
+    ) -> Result<(), TransportError<BulkOnlyError>> {
+        rprintln!("Handling: scsi cmd");
+
+        match command.kind {
+            ScsiCommand::TestUnitReady { .. } => {
+                command.pass();
+            }
+            ScsiCommand::Inquiry { .. } => {
+                command.try_write_data_all(&[
+                    0x00, // periph qualifier, periph device type
+                    0x80, // Removable
+                    0x04, // SPC-2 compliance
+                    0x02, // NormACA, HiSu, Response data format
+                    0x20, // 36 bytes in total
+                    0x00, // additional fields, none set
+                    0x00, // additional fields, none set
+                    0x00, // additional fields, none set
+                    b'U', b'N', b'K', b'N', b'O', b'W', b'N', b' ', // 8-byte T-10 vendor id
+                    b'S', b'T', b'M', b'3', b'2', b' ', b'U', b'S', b'B', b' ', b'F', b'l', b'a',
+                    b's', b'h', b' ', // 16-byte product identification
+                    b'1', b'.', b'2', b'3', // 4-byte product revision
+                ])?;
+                command.pass();
+            }
+            ScsiCommand::RequestSense { .. } => unsafe {
+                command.try_write_data_all(&[
+                    0x70,                         // RESPONSE CODE. Set to 70h for information on current errors
+                    0x00,                         // obsolete
+                    STATE.sense_key.unwrap_or(0), // Bits 3..0: SENSE KEY. Contains information describing the error.
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00, // INFORMATION. Device-specific or command-specific information.
+                    0x00, // ADDITIONAL SENSE LENGTH.
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,                               // COMMAND-SPECIFIC INFORMATION
+                    STATE.sense_key_code.unwrap_or(0),  // ASC
+                    STATE.sense_qualifier.unwrap_or(0), // ASCQ
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                ])?;
+                STATE.reset();
+                command.pass();
+            },
+            ScsiCommand::ReadCapacity10 { .. } => {
+                let mut data = [0u8; 8];
+                let _ = &mut data[0..4].copy_from_slice(&u32::to_be_bytes(BLOCKS - 1));
+                let _ = &mut data[4..8].copy_from_slice(&u32::to_be_bytes(BLOCK_SIZE));
+                command.try_write_data_all(&data)?;
+                command.pass();
+            }
+            ScsiCommand::ReadCapacity16 { .. } => {
+                let mut data = [0u8; 16];
+                let _ = &mut data[0..8].copy_from_slice(&u32::to_be_bytes(BLOCKS - 1));
+                let _ = &mut data[8..12].copy_from_slice(&u32::to_be_bytes(BLOCK_SIZE));
+                command.try_write_data_all(&data)?;
+                command.pass();
+            }
+            ScsiCommand::ReadFormatCapacities { .. } => {
+                let mut data = [0u8; 12];
+                let _ = &mut data[0..4].copy_from_slice(&[
+                    0x00, 0x00, 0x00, 0x08, // capacity list length
+                ]);
+                let _ = &mut data[4..8].copy_from_slice(&u32::to_be_bytes(BLOCKS as u32)); // number of blocks
+                data[8] = 0x01; //unformatted media
+                let block_length_be = u32::to_be_bytes(BLOCK_SIZE);
+                data[9] = block_length_be[1];
+                data[10] = block_length_be[2];
+                data[11] = block_length_be[3];
+
+                command.try_write_data_all(&data)?;
+                command.pass();
+            }
+            ScsiCommand::Read { lba, len } => unsafe {
+                let lba = lba as u32;
+                let len = len as u32;
+                if STATE.storage_offset != (len * BLOCK_SIZE) as usize {
+                    let start = (BLOCK_SIZE * lba) as usize + STATE.storage_offset;
+                    let end = (BLOCK_SIZE * lba) as usize + (BLOCK_SIZE * len) as usize;
+
+                    // Uncomment this in order to push data in chunks smaller than a USB packet.
+                    // let end = min(start + USB_PACKET_SIZE as usize - 1, end);
+
+                    rprintln!("Data transfer >>>>>>>>");
+                    let count = command.write_data(&mut STORAGE[start..end])?;
+                    STATE.storage_offset += count;
+                } else {
+                    command.pass();
+                    STATE.storage_offset = 0;
+                }
+            },
+            ScsiCommand::Write { lba, len } => unsafe {
+                let lba = lba as u32;
+                let len = len as u32;
+                if STATE.storage_offset != (len * BLOCK_SIZE) as usize {
+                    let start = (BLOCK_SIZE * lba) as usize + STATE.storage_offset;
+                    let end = (BLOCK_SIZE * lba) as usize + (BLOCK_SIZE * len) as usize;
+                    rprintln!("Data transfer <<<<<<<<");
+                    let count = command.read_data(&mut STORAGE[start..end])?;
+                    STATE.storage_offset += count;
+
+                    if STATE.storage_offset == (len * BLOCK_SIZE) as usize {
+                        command.pass();
+                        STATE.storage_offset = 0;
+                    }
+                } else {
+                    command.pass();
+                    STATE.storage_offset = 0;
+                }
+            },
+            ScsiCommand::ModeSense6 { .. } => {
+                command.try_write_data_all(&[
+                    0x03, // number of bytes that follow
+                    0x00, // the media type is SBC
+                    0x00, // not write-protected, no cache-control bytes support
+                    0x00, // no mode-parameter block descriptors
+                ])?;
+                command.pass();
+            }
+            ScsiCommand::ModeSense10 { .. } => {
+                command.try_write_data_all(&[0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])?;
+                command.pass();
+            }
+            ref unknown_scsi_kind => {
+                rprintln!("Unknown SCSI command");
+                unsafe {
+                    STATE.sense_key.replace(0x05); // illegal request Sense Key
+                    STATE.sense_key_code.replace(0x20); // Invalid command operation ASC
+                    STATE.sense_qualifier.replace(0x00); // Invalid command operation ASCQ
+                }
+                command.fail();
+            }
+        }
+
+        Ok(())
     }
 }
